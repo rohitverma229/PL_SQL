a1df920ae6a5dd2fd572aa15d34e44d19d90d4ff
Rohit Verma <48760327+rohitverma229@users.noreply.github.com>
2019-08-12 20:04:01 +0530

    new saved

--- Numeri_Loop.sql
+++ Numeri_Loop.sql
@@ -15,3 +15,16 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE(v_counter);
     END LOOP;
 END;
+/
+SET SERVEROUTPUT ON;
+DECLARE
+    v_num NUMBER:= &ENTER_NUMBER;
+    v_counter NUMBER;
+    v_result NUMBER:=0;
+BEGIN
+    FOR v_counter IN 1 .. 10 
+    LOOP
+        v_result:= v_counter * v_num;
+        DBMS_OUTPUT.PUT_LINE(v_counter|| ' X ' ||v_num);
+    END LOOP;
+END;
